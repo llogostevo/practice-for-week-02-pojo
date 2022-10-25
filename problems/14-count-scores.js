cd4673fab691d8877c9959fa8f3f0bb8379b9eb0
Lloyd Stevens <stevens.lloyd@gmail.com>
2022-10-25 17:43:40 +0100

    problem 14 solved, not destructed

--- problems/14-count-scores.js
+++ problems/14-count-scores.js
@@ -27,9 +27,51 @@ let peeps = [
 console.log(countScores(peeps)); //=> { Anthony: 4, Fred: 4, Winnie: 6 }
 ***********************************************************************/
 
+
+function matchKey(obj, searchKey){
+  for (let key in obj){
+    if (key == searchKey){
+      return true;
+    }
+  } return false;
+}
+
 function countScores(people) {
   // Your code here
+  let scores = {};
+  for(let person in people){
+    if (!(people[person].name in scores)){
+      scores[people[person].name]=0;
+    }
+  }
+
+  for(person in people){
+    if ((people[person].name in scores)){
+      scores[people[person].name]=scores[people[person].name]+people[person].score;
+    }
+  }
+  return scores;
 }
+
+
+
+let ppl = [{name: "Anthony", score: 10},
+            {name: "Fred", score : 10},
+            {name: "Anthony", score: -8},
+            {name: "Winnie", score: 12}];
+
+console.log(countScores(ppl)); //=> { Anthony: 2, Fred: 10, Winnie: 12 }
+
+let peeps = [
+  {name: "Anthony", score: 2},
+  {name: "Winnie", score: 2},
+  {name: "Fred", score: 2},
+  {name: "Winnie", score: 2},
+  {name: "Fred", score: 2},
+  {name: "Anthony", score: 2},
+  {name: "Winnie", score: 2}
+];
+console.log(countScores(peeps)); //=> { Anthony: 4, Fred: 4, Winnie: 6 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = countScores;
