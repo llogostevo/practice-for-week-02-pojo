36778db51bdb93a9d405e335e65443607f34ff29
Lloyd Stevens <stevens.lloyd@gmail.com>
2022-10-25 12:23:10 +0100

    problem 7 complete

--- problems/07-break-down-object.js
+++ problems/07-break-down-object.js
@@ -15,8 +15,23 @@ breakDownObj(object2); // => [ 'location', 'borough', 'NY', 'Brooklyn' ]
 ***********************************************************************/
 
 function breakDownObj(obj) {
-  // Your code here
+  // set an empty array, then use the spread syntax to append the key to the new array on each passing. 
+  let keys = [];
+  for (const key in obj){
+    keys = [...keys, key];
+  }
+
+  for (const key in obj){
+    keys = [...keys, obj[key]];
+  }
+  return keys;
 }
+
+let object1 = {name: 'Rupert', age: 5, speak: 'Meow'};
+breakDownObj(object1); // => [ 'name', 'age', 'speak', 'Rupert', 5, 'Meow' ]
+
+let object2 = {location: 'NY', borough: 'Brooklyn'};
+breakDownObj(object2); // => [ 'location', 'borough', 'NY', 'Brooklyn' ]
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = breakDownObj;
